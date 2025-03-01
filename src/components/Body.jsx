a98cd70c00e2cde7ff2b49d44bb14b95d9fd7308
VaibhavVe <vaibhavverma0620@gmail.com>
2025-03-01 15:29:02 +0530

    second

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,5 @@
 import Login from "./Login"
 import Browse from "./Browse";
-import Header from "./Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
@@ -8,10 +7,8 @@ const Body = () => {
   return (
     <BrowserRouter>
     <div>
-      <Header/>
       <Routes>
-      <Route path = "/" element = {<Body/>}  />
-      <Route path = "/Login" element = {<Login/>}  />
+      <Route path = "/" element = {<Login/>}  />
       <Route path = "/Browse" element = {<Browse/>}  />
       </Routes>
       </div> 
